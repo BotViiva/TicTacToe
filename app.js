9d9cb422f00e7ce1fa9b2be9f2838553ced7889f
Jaakko <jaakko.niininen@gmail.com>
2021-03-16 19:17:11 +0200

    valmis mutta buginen

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const boxes = Array.from(document.getElementsByClassName('box'));
-
+const playText = document.getElementById('playText');
+const restartBtn = document.getElementById('restartBtn');
+const spaces = [];
 const O_TEXT = "0";
 const X_TEXT = "X";
-let currentPlayer = O_TEXT;
+let currentPlayer;
 
 const drawBoard = () => {
     boxes.forEach((box, index) => {
@@ -25,7 +27,67 @@ const drawBoard = () => {
 };
 
 const boxClicked = (e) => {
-    console.log('box was clicked')
+    const id = e.target.id;
+    if (!spaces[id]) {
+        spaces[id] = currentPlayer;
+        e.target.innerText = currentPlayer;
+        if (playerHasWon()) {
+            playText.innerText = `${currentPlayer} has won!`;
+            return;
+        }
+        currentPlayer = currentPlayer === O_TEXT ? X_TEXT : O_TEXT;
+    }
 }
 
+const playerHasWon = () => {
+    if (spaces[0] = currentPlayer) {
+        if (spaces[1] === currentPlayer && spaces[2] === currentPlayer) {
+            console.log(`${currentPlayer} wins up top.`);
+            return true;
+        };
+        if (spaces[3] === currentPlayer && spaces[6] === currentPlayer) {
+            console.log(`${currentPlayer} wins left row.`);
+            return true;
+        };
+        if (spaces[4] === currentPlayer && spaces[8] === currentPlayer) {
+            console.log(`${currentPlayer} wins diagonally.`);
+            return true;
+        };
+    };
+    if (spaces[8] = currentPlayer) {
+        if (spaces[2] === currentPlayer && spaces[5] === currentPlayer) {
+            console.log(`${currentPlayer} wins right row.`);
+            return true;
+        };
+        if (spaces[6] === currentPlayer && spaces[7] === currentPlayer) {
+            console.log(`${currentPlayer} wins bottom.`);
+            return true;
+        };
+    };
+    if (spaces[4] === currentPlayer) {
+        if (spaces[1] === currentPlayer && spaces[7] === currentPlayer) {
+            console.log(`${currentPlayer} wins vertically.`);
+            return true;
+        };
+        if (spaces[3] === currentPlayer && spaces[5] === currentPlayer) {
+            console.log(`${currentPlayer} wins horizontally.`);
+            return true;
+        };
+    };
+};
+
+const restart = () => {
+    spaces.forEach((space, index) => {
+        spaces[index] = null;
+    })
+    boxes.forEach(box => {
+        box.innerText = '';
+    })
+    playText.innerText = `Let's play!`;
+    currentPlayer = O_TEXT;
+}
+
+restartBtn.addEventListener('click', restart);
+
+restart();
 drawBoard();
